e719f43e8065196e5af44d32508678681d573bd3
javis250 <61563583+javis250@users.noreply.github.com>
2020-05-29 23:54:42 -0500

    Add files via upload

---
+++ pro_fin2/keys.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+
+    database: {
+        host: 'sql3.freemysqlhosting.net',
+        user: 'sql3344246',
+        password: 'Rt8m4ApDHp',
+        database: 'sql3344246'
+    }
+
+};
